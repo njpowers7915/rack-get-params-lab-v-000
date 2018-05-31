fa26445290713b52d29dc5f0a5eced14733ec041
Nicholas Powers <njpowers7915@gmail.com>
2018-05-31 16:46:04 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      if @@cart = []
+      if @@cart == []
         resp.write "Your cart is empty"
       else
         @@cart.each do |i|
